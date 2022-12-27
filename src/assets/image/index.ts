83e49d7b8cba01f4a6009ae18c00c58d2ad9605f
Чайковский Кристиан Андреевич <tchaikovsky.md@gmail.com>
2022-12-27 16:15:25 +0200

    Add icons

---
+++ src/assets/image/index.ts
@@ -0,0 +1,25 @@
+import { ReactComponent as Add } from "./ic-add.svg";
+import { ReactComponent as Eye } from "./ic-eye.svg";
+import { ReactComponent as Input } from "./ic-input.svg";
+import { ReactComponent as Person } from "./ic-person.svg";
+import { ReactComponent as Search } from "./ic-search.svg";
+import { ReactComponent as Create } from "./ic-create.svg";
+import { ReactComponent as Delete } from "./ic-delete.svg";
+import { ReactComponent as Profile } from "./ic-profile.svg";
+import { ReactComponent as CloseEye } from "./ic-close-eye.svg";
+import { ReactComponent as PersonGroup } from "./ic-group-person.svg";
+
+export const icons = {
+    add: Add,
+    eye: Eye,
+    input: Input,
+    person: Person,
+    search: Search,
+    create: Create,
+    delete: Delete,
+    profile: Profile,
+    close_eye: CloseEye,
+    person_group: PersonGroup
+};
+
+export type IconKeys = keyof typeof icons;
