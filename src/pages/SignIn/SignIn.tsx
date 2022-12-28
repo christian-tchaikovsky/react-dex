97dfcde8a547efe1baefb1a7c3fde64cdc885838
Чайковский Кристиан Андреевич <tchaikovsky.md@gmail.com>
2022-12-28 16:21:58 +0200

    Authorization form template

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from "react";
+import { Link } from "@/components/UI/Link";
 import { Input } from "@/components/UI/Input";
 import { Button } from "@/components/UI/Button";
 import { Password } from "@/components/UI/Password";
+import { Typography } from "@/components/UI/Typography";
 import { SubmitHandler, useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import Banner from "@/assets/image/im-sign-up-banner.png";
 import styles from "./SignIn.module.sass";
 
@@ -11,8 +15,24 @@ interface Inputs {
     password: string
 }
 
+const validationSchema = Yup.object<Record<keyof Inputs, Yup.AnySchema>>({
+    login: Yup.string()
+        .trim()
+        .required("Required")
+        .matches(/^[a-zA-Z]+$/, "Invalid character entered")
+        .min(5, "Login must be at least 5 characters")
+        .max(24, "Login must be at most 24 characters"),
+    password: Yup.string()
+        .trim()
+        .required("Required")
+        .matches(/^[a-zA-Z]+$/, "Invalid character entered")
+        .min(5, "Password must be at least 5 characters")
+        .max(24, "Password must be at most 24 characters")
+});
+
 export const SignIn: FC = () => {
     const { control, handleSubmit } = useForm<Inputs>({
+        resolver: yupResolver(validationSchema),
         defaultValues: {
             login: "",
             password: ""
@@ -25,6 +45,7 @@ export const SignIn: FC = () => {
         <div className={styles.auth}>
             <div className={styles.left}>
                 <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                    <Typography tag="h1" size="large" className={styles.title}>Sign In</Typography>
                     <Controller
                         control={control}
                         name="login"
@@ -54,6 +75,10 @@ export const SignIn: FC = () => {
                         )}
                     />
                     <Button type="submit" fullWidth>Sign In</Button>
+                    <div className={styles.bottom}>
+                        <Typography className={styles.typography} color="secondary">Not a member yet?</Typography>
+                        <Link className={styles.link} to="/register">Sign up</Link>
+                    </div>
                 </form>
             </div>
             <div className={styles.right}>
