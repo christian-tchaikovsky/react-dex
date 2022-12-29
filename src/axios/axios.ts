7d34d9670362f6a17230b4c2cac3213874e4db7a
Чайковский Кристиан Андреевич <tchaikovsky.md@gmail.com>
2022-12-29 10:11:52 +0200

    Axios init

---
+++ src/axios/axios.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const HTTP = axios.create({ baseURL: process.env.REACT_APP_API_ENDPOINT });
+
+HTTP.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token");
+
+        if (config.headers && token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    async error => await Promise.reject(error)
+);
+
+HTTP.interceptors.response.use(
+    response => response,
+    async error => {
+
+        if (error.response.status === 401) {
+            // TODO Redirect to "/login"
+        }
+
+        return await Promise.reject(error);
+    }
+);
+
+export default HTTP;
