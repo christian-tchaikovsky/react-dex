882a070d71d4ae0e29e99f26bed11c89f48904c1
Baranovskaya Ekaterina <baranovskaae19@gmail.com>
2022-12-31 19:14:13 +0200

    Checkbox error state

--- src/components/UI/Checkbox/Checkbox.tsx
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -7,19 +7,23 @@ interface Props {
     value: boolean
     onChange: React.ChangeEventHandler<HTMLInputElement>
     className?: string
+    error?: string
 
     // all other props
     [x: string]: any
 }
 
 export const Checkbox: FC<Props> = (props) => {
-    const { label, value, onChange, className, ...rest } = props;
+    const { label, value, onChange, className, error, ...rest } = props;
     
     return (
-        <label className={classNames(styles.checkbox, className)} {...rest}>
-            <span className={classNames(styles.mark, { [styles.checked]: value })}/>
-            <input type="checkbox" checked={value} onChange={onChange} />
-            {label}
-        </label>
+        <div className={classNames(styles.checkbox, className)} {...rest}>
+            <label className={classNames(styles.label, { [styles.error]: error })}>
+                <span className={classNames(styles.mark, { [styles.checked]: value })}/>
+                <input type="checkbox" checked={value} onChange={onChange} />
+                {label}
+            </label>
+            {error && <span className={styles.error}>{error}</span>}
+        </div>
     );
 };
