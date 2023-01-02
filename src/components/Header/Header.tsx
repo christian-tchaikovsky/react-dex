593b1ff05046c4005aaaab43958dd7b3860ae9dd
Baranovskaya Ekaterina <baranovskaae19@gmail.com>
2023-01-02 09:29:58 +0200

    Change sidebar component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,24 @@
 import React, { FC } from "react";
 import { Icon } from "@/components/Icon";
 import { Typography } from "@/components/UI/Typography";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { IUser } from "@/models/IUser";
 import styles from "./Header.module.sass";
 
 export const Header: FC = () => {
-    const navigate = useNavigate();
     const user: IUser = JSON.parse(localStorage.getItem("user") as string);
-
-    function onHandleClick(): void {
-        navigate("/profile");
-    }
 
     return (
         <header className={styles.header}>
             <Icon className={styles.logo} name="logo"/>
             <div className={styles.user}>
                 <Typography className={styles.name} size="small">{user.name}</Typography>
-                <div className={styles.avatar}>
+                <Link className={styles.avatar} to="/profile">
                     {user.avatarUrl
-                        ? <img onClick={onHandleClick} className={styles.image} src={user.avatarUrl} alt="avatar"/>
-                        : <Icon onClick={onHandleClick} className={styles.image} name="profile"/>
+                        ? <img className={styles.image} src={user.avatarUrl} alt="avatar"/>
+                        : <Icon className={styles.image} name="profile"/>
                     }
-                </div>
+                </Link>
             </div>
         </header>
     );
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -4,31 +4,33 @@ import styles from "./SideBar.module.sass";
 
 export const SideBar: FC = (): JSX.Element => (
     <header className={styles.sideBar}>
-        <div className={styles.main}>
-            <Anchor
-                to="/teams"
-                title="Главная"
-                icon="person_group"
-            >
-                    Teams
-            </Anchor>
-            <Anchor
-                to="/players"
-                title="Операции"
-                icon="person"
-            >
-                    Players
-            </Anchor>
-        </div>
-        <div className={styles.rest}>
-            <Anchor
-                to="/logout"
-                variant="secondary"
-                title="Профиль"
-                icon="input"
-            >
-                    Sign out
-            </Anchor>
-        </div>
+        <nav className={styles.navigation}>
+            <div className={styles.main}>
+                <Anchor
+                    to="/teams"
+                    title="Главная"
+                    icon="person_group"
+                >
+                   Teams
+                </Anchor>
+                <Anchor
+                    to="/players"
+                    title="Операции"
+                    icon="person"
+                >
+                   Players
+                </Anchor>
+            </div>
+            <div className={styles.rest}>
+                <Anchor
+                    to="/logout"
+                    variant="secondary"
+                    title="Профиль"
+                    icon="input"
+                >
+                   Sign out
+                </Anchor>
+            </div>
+        </nav>
     </header>
 );
