4b9b9aee2e7131ab33614bdff778f8d5dca38693
Christopher <tchaikovsky.md@gmail.com>
2023-01-23 08:32:48 +0200

    Add new hook (use-match-media)

---
+++ src/common/constants/queries.ts
@@ -0,0 +1,11 @@
+const mobile = "(max-width: 766px)";
+
+const tablet = "(min-width: 767px) and (max-width: 1199px)";
+
+const desktop = "(min-width: 1200px)";
+
+export {
+    mobile,
+    tablet,
+    desktop
+};
