e695d635685443e1c07b3c954c12d5c20683e331
Christopher <tchaikovsky.md@gmail.com>
2023-01-23 14:11:24 +0200

    Full adaptation

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,15 +4,20 @@ import { Typography } from "@/common/components/UI/Typography";
 import { Condition } from "@/common/components/Condition";
 import { IUser } from "@/common/interfaces/IUser";
 import { Icon } from "@/common/components/Icon";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { paths } from "@/routes/paths";
 import styles from "./Header.module.sass";
 import classNames from "classnames";
 
 export const Header: FC = () => {
-    const user: IUser = JSON.parse(localStorage.getItem("user") as string);
+    const navigate = useNavigate();
     const { mobile, onChangeHandler } = useNavigationContext();
+    const user: IUser = JSON.parse(localStorage.getItem("user") as string);
 
+    const onHandleNavigate = (): void => {
+        navigate(paths.main);
+    };
+    
     return (
         <header className={styles.header}>
             <Condition condition={mobile} otherwise={null}>
@@ -23,7 +28,11 @@ export const Header: FC = () => {
                     <div className={styles["button-menu"]} />
                 </div>
             </Condition>
-            <Icon className={classNames(styles.logo, { [styles.center]: mobile })} name="logo"/>
+            <Icon
+                name="logo"
+                onClick={onHandleNavigate}
+                className={classNames(styles.logo, { [styles.center]: mobile })}
+            />
             <Condition condition={!mobile} otherwise={null}>
                 <div className={styles.user}>
                     <Typography className={styles.name} size="small">{user.name}</Typography>
